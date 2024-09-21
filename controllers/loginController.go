36c0e934849e99ba9082ce788edd5a4b4d615474
Akshit <akshit10.anand2004@gmail.com>
2024-09-21 16:40:16 +0530

    feat: Add submit complaint route to store PNR and image files in bucket

---
+++ controllers/loginController.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"mime/multipart"
+
+	"github.com/gofiber/fiber/v2"
+	"rail-madad/config"
+)
+
+type PNRRequest struct {
+	PNR   string                `json:"pnr"`
+	Image *multipart.FileHeader `form:"image"`
+}
+
+func UploadImageToBucket(file *multipart.FileHeader) (string, error) {
+	ctx := context.Background()
+
+	bucketName := os.Getenv("GCS_BUCKET_NAME")
+	// fmt.Println("bucketName", bucketName)
+	if bucketName == "" {
+		return "", fmt.Errorf("bucket name is not configured")
+	}
+
+	bucket := config.StorageClient.Bucket(bucketName)
+
+	src, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	data, err := ioutil.ReadAll(src)
+	if err != nil {
+		return "", err
+	}
+
+	obj := bucket.Object("images/" + file.Filename)
+	writer := obj.NewWriter(ctx)
+	defer writer.Close()
+
+	if _, err := writer.Write(data); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("https://storage.googleapis.com/%s/images/%s", bucketName, file.Filename), nil
+}
+
+func SubmitPNR(c *fiber.Ctx) error {
+	var pnrRequest PNRRequest
+
+	pnrRequest.PNR = c.FormValue("pnr")
+	image, err := c.FormFile("image")
+	if err != nil {
+		log.Printf("Error getting image: %v", err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to get image from request",
+		})
+	}
+	pnrRequest.Image = image
+
+	log.Printf("Received PNR: %s", pnrRequest.PNR)
+
+	log.Println("Image provided in the request")
+	imageURL, err := UploadImageToBucket(pnrRequest.Image)
+	if err != nil {
+		log.Printf("Error uploading image: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to upload image",
+		})
+	}
+
+	_, err = config.FirestoreClient.Collection("pnrs").Doc(pnrRequest.PNR).Set(context.Background(), map[string]interface{}{
+		"pnr":       pnrRequest.PNR,
+		"image_url": imageURL,
+	})
+	if err != nil {
+		log.Printf("Error storing PNR: %v", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to store PNR",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message":   "PNR submitted successfully",
+		"pnr":       pnrRequest.PNR,
+		"image_url": imageURL,
+	})
+}
---
+++ routes/loginRoute.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+    "github.com/gofiber/fiber/v2"
+	"rail-madad/controllers"
+)
+
+func SetupLoginRoutes(app *fiber.App) {
+    app.Post("/submit-complaint", controllers.SubmitPNR)
+}
